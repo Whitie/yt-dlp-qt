478e3968649c54f9a948595109fe8e5e51a0bf98
Whitie <weimann.th@gmail.com>
2024-12-22 10:11:54 +0100

    Included ui loader in main file.

--- yt_dlp_qt/main.py
+++ yt_dlp_qt/main.py
@@ -8,8 +8,7 @@ from threading import Event
 import yt_dlp
 
 from PySide6 import QtCore, QtGui, QtWidgets
-
-from uic import loadUi
+from PySide6.QtUiTools import QUiLoader
 
 
 _PATH = Path(__file__).resolve().parent
@@ -19,6 +18,46 @@ AUDIO_CODECS = ("aac", "vorbis", "mp3", "opus", "wav")
 AUDIO_QUALITIES = (0, 2, 5, 8)
 VIDEO_CONTAINERS = ("mp4", "mkv", "3gp", "flv", "webm")
 VIDEO_RESOLUTIONS = (2160, 1440, 1080, 720, 480)
+
+
+class UiLoaderError(Exception):
+    pass
+
+
+class UiLoader(QUiLoader):
+
+    def __init__(self, base, custom_widgets=None):
+        QUiLoader.__init__(self, base)
+        self.base = base
+        self.custom_widgets = custom_widgets
+
+    def createWidget(self, class_name, parent=None, name=""):
+        if parent is None and self.base:
+            return self.base
+        else:
+            if class_name in self.availableWidgets():
+                widget = QUiLoader.createWidget(self, class_name, parent, name)
+            else:
+                try:
+                    widget = self.custom_widgets[class_name](parent)
+                except (TypeError, KeyError):
+                    raise UiLoaderError(
+                        f'No custom widget "{class_name}" found.'
+                    )
+            if self.base:
+                setattr(self.base, name, widget)
+            return widget
+
+
+def loadUi(ui_file, base=None, custom_widgets=None, working_directory=None):
+    loader = UiLoader(base, custom_widgets)
+    if working_directory is not None:
+        loader.setWorkingDirectory(working_directory)
+    if not isinstance(ui_file, str):
+        ui_file = str(ui_file)
+    widget = loader.load(ui_file)
+    QtCore.QMetaObject.connectSlotsByName(widget)
+    return widget
 
 
 class Worker(QtCore.QThread):
